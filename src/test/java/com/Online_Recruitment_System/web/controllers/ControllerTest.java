0ec8ef43ceca2c12c0121a29fc1d448308009156
Randsafi <rands068@gmail.com>
2024-05-10 01:00:17 +0300

    add database MySql

---
+++ src/test/java/com/Online_Recruitment_System/web/controllers/ControllerTest.java
@@ -0,0 +1,10 @@
+package com.Online_Recruitment_System.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tex")
+class ControllerTest {
+
+}
