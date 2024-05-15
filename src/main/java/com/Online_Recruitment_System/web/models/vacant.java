8dc3b98fe686e258b5a8229c8179fac2b532c68e
Randsafi <rands068@gmail.com>
2024-05-15 22:12:02 +0300

    add database MySql

--- src/main/java/com/Online_Recruitment_System/web/models/vacant.java
+++ src/main/java/com/Online_Recruitment_System/web/models/vacant.java
@@ -1,21 +1,27 @@
 package com.Online_Recruitment_System.web.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jdk.jfr.Enabled;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Enabled
-@Table(name="vacant")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "vacant")
 public class vacant {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String Job_type;
     private String Description;
     private  int Number_years_experience;
     private String Work_place;
     private  int Holidays;
+
+
 
 }
